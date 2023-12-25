ffb020eada17a2d72c6a0e5f0ef52eaabb8fcb38
Michael Leo <yhzemail61010@aliyun.com>
2023-12-25 11:10:51 +0800

    Add more utils.

--- feature_base/src/main/kotlin/com/leovp/module/common/presentation/compose/ext/ModifierExt.kt
+++ feature_base/src/main/kotlin/com/leovp/module/common/presentation/compose/ext/ModifierExt.kt
@@ -1,3 +1,5 @@
+@file:Suppress("unused", "MemberVisibilityCanBePrivate")
+
 package com.leovp.module.common.presentation.compose.ext
 
 import androidx.compose.foundation.clickable
@@ -5,6 +7,12 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 
 /**
  * Author: Michael Leo
@@ -18,3 +26,30 @@ fun Modifier.noRippleClickable(onClick: () -> Unit) = composed {
         onClick = onClick
     )
 }
+
+/**
+ * Usage:
+ * ```
+ * Row(modifier = Modifier.bottomBorder(DarkGray, 1.d)) { }
+ * ```
+ *
+ * https://stackoverflow.com/a/68595142
+ */
+fun Modifier.bottomBorder(color: Color, strokeWidth: Dp = 1.dp) = composed(
+    factory = {
+        val density = LocalDensity.current
+        val strokeWidthPx = density.run { strokeWidth.toPx() }
+
+        Modifier.drawBehind {
+            val width = size.width
+            val height = size.height - strokeWidthPx / 2
+
+            drawLine(
+                color = color,
+                start = Offset(x = 0f, y = height),
+                end = Offset(x = width, y = height),
+                strokeWidth = strokeWidthPx
+            )
+        }
+    }
+)
