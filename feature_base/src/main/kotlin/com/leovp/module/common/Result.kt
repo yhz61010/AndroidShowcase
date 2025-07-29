6fd035bf9f52968b3e3ed57ac473014d124cab43
Michael Leo <yhzemail61010@aliyun.com>
2025-07-29 15:25:16 +0800

    Add more method in Result.kt

--- feature_base/src/main/kotlin/com/leovp/module/common/Result.kt
+++ feature_base/src/main/kotlin/com/leovp/module/common/Result.kt
@@ -70,6 +70,8 @@ fun <T> Result<T>.exceptionOrNull(): ApiException? = when {
     else -> null
 }
 
+fun <T> Result<T>.exception(): ApiException = (this as Result.Failure).exception
+
 inline fun <T> Result<T>.onSuccess(action: (value: T) -> Unit): Result<T> {
     if (isSuccess) action((this as Result.Success<T>).data)
     return this
@@ -110,7 +112,7 @@ suspend inline fun <reified R> result(
     }.getOrElse { err ->
         var code = -65535
         var message: String? = err.message
-        var exception: Throwable = err
+        val exception: Throwable = err
 
         // err can be one of the following exception:
         // - RequestParamsException
