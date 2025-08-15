d9cf49dbd44f13438902fcfa35b1b4e17f9eda2e
Michael Leo <yhzemail61010@aliyun.com>
2025-08-15 10:44:08 +0800

    Upgrade all dependencies to the latest version.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,13 +13,6 @@ pluginManagement {
      * look for its dependencies.
      */
     repositories {
-        maven { url = uri("https://maven.aliyun.com/repository/google") }
-        // Tencent Gradle mirrors
-        maven { url = uri("https://mirrors.cloud.tencent.com/nexus/repository/gradle-plugins/") }
-        maven { url = uri("https://mirrors.tuna.tsinghua.edu.cn/maven") }
-        // AliYun Gradle mirrors
-        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
-
         gradlePluginPortal()
         google {
             content {
@@ -29,6 +22,13 @@ pluginManagement {
             }
         }
         mavenCentral()
+
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        // Tencent Gradle mirrors
+        maven { url = uri("https://mirrors.cloud.tencent.com/nexus/repository/gradle-plugins/") }
+        maven { url = uri("https://mirrors.tuna.tsinghua.edu.cn/maven") }
+        // AliYun Gradle mirrors
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
     }
 }
 
@@ -44,10 +44,21 @@ dependencyResolutionManagement {
      * but it does not configure any dependencies (unless you select a
      * template that requires some).
      */
-    @Suppress ("UnstableApiUsage")
+    @Suppress("UnstableApiUsage")
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    @Suppress ("UnstableApiUsage")
+
+    @Suppress("UnstableApiUsage")
     repositories {
+        google()
+        mavenCentral {
+            isAllowInsecureProtocol = true
+        }
+
+        maven("https://plugins.gradle.org/m2/")
+        maven("https://maven.java.net/content/groups/public/")
+        // https://github.com/airbnb/lottie/blob/master/android-compose.md
+        maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots/") }
+
         maven("https://jitpack.io")
 
         maven("https://maven.aliyun.com/repository/public")
@@ -56,16 +67,6 @@ dependencyResolutionManagement {
 
         maven("https://mirrors.cloud.tencent.com/gradle/")
         maven("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/")
-
-        maven("https://plugins.gradle.org/m2/")
-        maven("https://maven.java.net/content/groups/public/")
-        // https://github.com/airbnb/lottie/blob/master/android-compose.md
-        maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots/") }
-
-        google()
-        mavenCentral {
-            isAllowInsecureProtocol = true
-        }
     }
 }
 
